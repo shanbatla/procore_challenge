da9a365a6f09f97a7fb674921bf7a756e6b561ff
shanbatla <shan.batla@gmail.com>
2016-08-13 18:50:32 -0700

    Additional clean up and app restructuring.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
-import '../App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container-fluid">
+        <h2>Contacts Keeper</h2>
       </div>
     );
   }
