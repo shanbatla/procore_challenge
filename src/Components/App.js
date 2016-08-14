b6b6678a409aa8ce94331c485610cc5793fd86a8
shanbatla <shan.batla@gmail.com>
2016-08-14 16:53:24 -0700

    Add modal button component.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Header from './Header'
 import Table from './Table'
+import ModalButton from './ModalButton'
+import '../App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <ModalButton/>
         <Table/>
       </div>
     );
---
+++ src/Components/ModalButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class ModalButton extends Component {
+  render() {
+    return (
+      <div>
+        <button className="modal-button">+ Contacts Keeper</button>
+      </div>
+    );
+  }
+}
+
+export default ModalButton;
