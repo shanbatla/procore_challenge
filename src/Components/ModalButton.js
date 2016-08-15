446f3f719284de81be890c3de02475a95ca660be
shanbatla <shan.batla@gmail.com>
2016-08-14 17:49:22 -0700

    Set up modal forms.

--- src/Components/ModalButton.js
+++ src/Components/ModalButton.js
@@ -29,7 +29,31 @@ class ModalButton extends Component {
       <div>
         <button onClick={this.openModal} type="button" className="btn btn-primary modal-button">+ Contacts Keeper</button>
         <Modal isOpen={this.state.open}>
-          <button onClick={this.closeModal}>Close</button>
+          <div className="form-group">
+            <label for="first-name">First Name</label>
+            <input type="text" className="form-control" id="first-name"/>
+          </div>
+          <div className="form-group">
+            <label for="last-name">Last Name</label>
+            <input type="text" className="form-control" id="last-name"/>
+          </div>
+          <div className="form-group">
+            <label for="date-of-birth">Date of Birth</label>
+            <input type="text" className="form-control" id="date-of-birth"/>
+          </div>
+          <div className="form-group">
+            <label for="phone-number">Phone Number</label>
+            <input type="text" className="form-control" id="phone-number"/>
+          </div>
+          <div className="form-group">
+            <label for="adress">Address</label>
+            <input type="text" className="form-control" id="adress"/>
+          </div>
+          <div className="form-group">
+            <label for="notes">Notes</label>
+            <input type="text" className="form-control" id="notes"/>
+          </div>
+          <button onClick={this.closeModal} className="btn btn-primary modal-button">Save</button>
         </Modal>
       </div>
     );
